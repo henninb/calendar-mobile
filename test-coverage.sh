7804ead72e153146031997bef4b42888e9b4e792
Brian Henning <henninb@msn.com>
2026-05-08 05:54:10 -0500

    wip

--- test-coverage.sh
+++ test-coverage.sh
@@ -2,5 +2,8 @@
 
 flutter test --coverage
 lcov --summary coverage/lcov.info
+genhtml coverage/lcov.info -o coverage/html
+xdg-open coverage/html/index.html
+echo xdg-open coverage/html/index.html
 
 exit 0
